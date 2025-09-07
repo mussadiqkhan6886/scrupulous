c3058c9e9626ef587291e7c3bcc36709be115832
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-09-07 17:57:21 +0500

    fix bug

--- components/ui/card-stack.tsx
+++ components/ui/card-stack.tsx
@@ -1,4 +1,6 @@
 "use client";
+
+ /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useEffect, useState } from "react";
 import { motion } from "motion/react";
 import Image from "next/image";
