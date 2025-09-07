61ff864c284683d0aad6bf096dec7c0670cbbd3f
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-09-07 14:17:09 +0500

    make footer

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,9 @@ import Image from 'next/image'
 import { Projects } from '@/components/Projects';
 import Link from 'next/link';
 import { FiArrowUpRight } from "react-icons/fi";
+import {SocialIcon} from "react-social-icons"
+import { FaUser } from "react-icons/fa";
+
 
 const imageVariant = {
   hidden: {
@@ -53,17 +56,25 @@ const Home = () => {
 
 
       <div className="bg-secondary p-3  md:h-full md:row-span-10 lg:row-span-9 row-span-20 h-[500px] rounded-xl">4</div>
-      <div className="bg-secondary p-3  w-full  h-[300px] row-span-5 rounded">
+      <div className="bg-secondary p-3  w-full text-[21px]  h-[300px] row-span-5 rounded">
         <p>At Scrupulous, we believe in turning ideas into impactful digital experiences. As a freelancing agency, we specialize in web development, design, and creative digital solutions — built with precision, passion, and integrity. Our goal is simple: deliver work that not only looks good but also works seamlessly, helping you grow with confidence.</p>  
       </div>
       
       <Link target='_blank' href={"https://mussadiqkhan.vercel.app/hire-me"} className="bg-contact text-secondary  p-3  relative  h-[300px] row-span-5 rounded-xl hover:bg-text duration-500 ease-in-out">
         <FiArrowUpRight className='text-4xl absolute right-3 top-3'/>
-        <h2 className='text-[60px] font-extralight absolute bottom-3'>Contact me</h2>
+        <h2 className='text-[60px] font-extralight absolute bottom-3 italic'>Contact me</h2>
       </Link>
       
 
-      <div className="bg-secondary p-3  row-span-2 w-full rounded-xl">7</div>
+      <div className="bg-secondary p-3  flex justify-evenly items-center  row-span-2 w-full rounded-xl">
+        <SocialIcon label='github icon' network='github' bgColor='transparent' url='https://github.com/mussadiqkhan6886' />
+        <SocialIcon label='instagram icon' network='instagram' bgColor='transparent' url='https://www.instagram.com/the__scrupulous/?__pwa=1' />
+        <SocialIcon label='linkedin icon' network='linkedin' bgColor='transparent' url='https://www.linkedin.com/in/mussadiq-khan-2815aa34a/' />
+        <SocialIcon label='whatsapp icon' network='whatsapp' bgColor='transparent' url='https://wa.me/+923189328584' />
+        <Link href='https://mussadiqkhan.vercel.app'>
+          <FaUser className='text-xl' />
+        </Link>
+      </div>
     </main>
   )
 }
